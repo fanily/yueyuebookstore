60c66cf61ae698f19dc25d5e0fb687dce97328ae
JoeAngel <joe10330@gmail.com>
2016-07-18 19:17:05 +0800

    Update from yueyuebookstore-dev:master 844bf1c

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ var MustacheTemplate = (function(){
     };
 })();
 
-$.getJSON("config.json?t=1468418403", function(config) {
+$.getJSON("config.json?t=1468840558", function(config) {
 	var get_fanily_post = function(keyword, body, callback) {
 		$.ajax({
 			url : config.api+"search/lists/"+keyword,
